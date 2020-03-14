ba3dad5bb5e9247e0d680fe4fabbb0f4820a3cc2
kaizen-minded <melvin3ezell@gmail.com>
2020-03-14 12:50:47 -0400

    scrolling animation

--- js/main.js
+++ js/main.js
@@ -21,3 +21,21 @@ $(function() {
   });
   $(".open-menu").height($(window).height());
 });
+
+// Smooth Scrolling
+$(".cf a").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
